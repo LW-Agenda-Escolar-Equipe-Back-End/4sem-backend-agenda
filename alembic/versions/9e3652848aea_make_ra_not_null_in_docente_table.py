ce123ed6fafaf544acf911bb9a0ffcf82344a36c
Marcos Alves <marcos.a.lopes7701@gmail.com>
2025-11-24 17:46:26 -0300

    Format Alembic migration files to use double quotes

--- alembic/versions/9e3652848aea_make_ra_not_null_in_docente_table.py
+++ alembic/versions/9e3652848aea_make_ra_not_null_in_docente_table.py
@@ -12,8 +12,8 @@ import sqlalchemy as sa
 
 
 # revision identifiers, used by Alembic.
-revision: str = '9e3652848aea'
-down_revision: Union[str, Sequence[str], None] = 'ed6a7532c77c'
+revision: str = "9e3652848aea"
+down_revision: Union[str, Sequence[str], None] = "ed6a7532c77c"
 branch_labels: Union[str, Sequence[str], None] = None
 depends_on: Union[str, Sequence[str], None] = None
 
@@ -21,10 +21,10 @@ depends_on: Union[str, Sequence[str], None] = None
 def upgrade() -> None:
     """Upgrade schema."""
     # Alternar a coluna ra para NOT NULL
-    op.alter_column('docente', 'ra', existing_type=sa.String(length=13), nullable=False)
+    op.alter_column("docente", "ra", existing_type=sa.String(length=13), nullable=False)
 
 
 def downgrade() -> None:
     """Downgrade schema."""
     # Reverter a coluna ra para nullable
-    op.alter_column('docente', 'ra', existing_type=sa.String(length=13), nullable=True)
+    op.alter_column("docente", "ra", existing_type=sa.String(length=13), nullable=True)
